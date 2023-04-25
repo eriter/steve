7e149947d208650d8a48f58820b9f5879b2a4b49
eriter <eli.riter@gmail.com>
2023-04-25 15:00:16 -0700

    bundler scaffolding

---
+++ lib/steve/version.rb
@@ -0,0 +1,3 @@
+module Steve
+  VERSION = "0.1.0"
+end
---
+++ spec/steve_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Steve do
+  it "has a version number" do
+    expect(Steve::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
